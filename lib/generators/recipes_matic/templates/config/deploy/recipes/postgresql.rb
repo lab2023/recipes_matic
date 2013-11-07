a3cd0e1f7efa6965d2f1c364e509c1f87a6e3b3f
Muhammet DİLEK <dilekmuhammet@gmail.com>
2013-11-07 17:34:04 +0200

    #5 postgresql setup fixed

--- lib/generators/recipes_matic/templates/config/deploy/recipes/postgresql.rb
+++ lib/generators/recipes_matic/templates/config/deploy/recipes/postgresql.rb
@@ -10,7 +10,7 @@ namespace :postgresql do
   task :install, roles: :db, only: {primary: true} do
     run "#{sudo} add-apt-repository -y ppa:pitti/postgresql"
     run "#{sudo} apt-get -y update"
-    run "#{sudo} apt-get -y install postgresql libpq-dev"
+    run "#{sudo} apt-get -y install postgresql-9.2 libpq-dev"
   end
 
   after "deploy:install", "postgresql:install"
